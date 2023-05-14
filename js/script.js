e6ce3a81a6627a5bf138a0f827e7fb4763c33b9c
Cael Foster <87586666+CaelFoster@users.noreply.github.com>
2023-05-14 23:17:37 +0000

    deleted javascript comments

--- js/script.js
+++ js/script.js
@@ -19,13 +19,14 @@ const data = [
     quote: `“Bringing colors to brighten our day”`, 
     emoji: '🌞', 
     image: `url(assets/gaillardia.jpeg)`
+  },
+  {
+    quote: `“As the sun shines down, they dance and sway.”`, 
+    emoji: ':D', 
+    image: `url(assets/rhododendron.jpeg)`
   }
 ];
 
-/*
-Quote and Emoji
-*/
-// blockquote
 const quote = document.querySelector('blockquote');
 // emoji
 const emoji = document.querySelector('p');
@@ -39,117 +40,82 @@ emoji.innerHTML = "👍️";
 // Check-Check: background image
 bgImage.style.backgroundImage= 'url(assets/HeartFlowerThingy.jpeg)';
 
-/*
- * Navigate to Items with Event Listeners
- * Track Number of Items
-*/
-// initialize item number
 let itemNumber = 0;
-// get next button
 const nextButton = document.querySelector("img.next");
-// get previous button
 const prevButton = document.querySelector("img.prev");
-// get random button
 const randButton = document.querySelector("img.random");
 
-// listen for clicks on next button
+
+
+
 nextButton.addEventListener("click", () => {
-  // update, then increase item number with each click
+
   itemNumber = itemNumber + 1;
   
-  // if at LAST item
   if ( itemNumber > data.length -1 ) {
-    // reset counting to first item
     itemNumber = 0;
   }
 
-  // display data based on array index
   quote.innerHTML = data[itemNumber].quote;
   emoji.innerHTML = data[itemNumber].emoji;
   bgImage.style.backgroundImage = data[itemNumber].image;
   
 });
   
-// listen for clicks on previous button
 prevButton.addEventListener("click", () => {
-  // update, then DEcrease item number with each click
   itemNumber = itemNumber - 1;
-  
-  // if at FIRST item
   if ( itemNumber < 0 ) {
-    // reset counting to last item
     itemNumber = data.length -1;
   }  
   
-  // display data based on array index
   quote.innerHTML = data[itemNumber].quote;
   emoji.innerHTML = data[itemNumber].emoji;
   bgImage.style.backgroundImage = data[itemNumber].image;
 });
 
-// listen for clicks on random button
 randButton.addEventListener('click', () => {
   
-  // generate random number, for total number, of items (data)
   randomNumber = Math.floor ( Math.random() * data.length );
   
-  // display data based random number
   quote.innerHTML = data[randomNumber].quote;
   emoji.innerHTML = data[randomNumber].emoji;
   bgImage.style.backgroundImage = data[randomNumber].image;
   
 });
 
-// listen for keypress next
 document.addEventListener('keyup', (event) => {
   
-  // check-check: What key was pressed?
-  // console.log(event);
-
-  // Next Right Arrow  
   if ( event.key === "ArrowRight" ) {
    
-   // update, then increase item number with each click
   itemNumber = itemNumber + 1;
   
-  // if at LAST item
   if ( itemNumber > data.length -1 ) {
-    // reset counting to first item
     itemNumber = 0;
   }
-
-  // display data based on array index
   quote.innerHTML = data[itemNumber].quote;
   emoji.innerHTML = data[itemNumber].emoji;
   bgImage.style.backgroundImage = data[itemNumber].image;
     
   }
   
-  // Prev Left Arrow
   if ( event.key === "ArrowLeft" ) {
   
-   // update, then DEcrease item number with each click
   itemNumber = itemNumber - 1;
   
-  // if at FIRST item
   if ( itemNumber < 0 ) {
-    // reset counting to last item
     itemNumber = data.length -1;
   }  
   
-  // display data based on array index
   quote.innerHTML = data[itemNumber].quote;
   emoji.innerHTML = data[itemNumber].emoji;
   bgImage.style.backgroundImage = data[itemNumber].image;
   }
   
-  // Random Spacebar
   if ( event.key === " ") {
     
-    // generate random number, for total number, of items (data)
     randomNumber = Math.floor ( Math.random() * data.length );
 
-    // display data based random number
+ 
     quote.innerHTML = data[randomNumber].quote;
     emoji.innerHTML = data[randomNumber].emoji;
     bgImage.style.backgroundImage = data[randomNumber].image;
